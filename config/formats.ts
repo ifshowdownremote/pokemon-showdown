cefcd071c173c10b647b2f346d20ca8efc58fe09
Pellevoisin <bouret.benjamin@gmail.com>
2020-07-18 10:07:15 +0100

    drone

--- config/formats.ts
+++ config/formats.ts
@@ -4,21 +4,21 @@ import {Utils} from './../lib/utils';
 
 export const Formats: (FormatsData | {section: string, column?: number})[] = [
 
-	// Sw/Sh Singles
+	// US/UM Singles
 	///////////////////////////////////////////////////////////////////
-
 	{
-		section: "Sw/Sh Singles",
+		section: "US/UM Singles",
 	},
-	
 	{
-		name: "[Gen 8] Anything Goes",
+		name: "[Gen 7] Anything Goes",
 		threads: [
-			`&bullet; <a href="https://www.smogon.com/forums/threads/3656317/">Anything Goes</a>`,
+			`&bullet; <a href="https://www.smogon.com/forums/threads/3587441/">Anything Goes Metagame Discussion</a>`,
+			`&bullet; <a href="https://www.smogon.com/forums/threads/3591711/">Anything Goes Viability Rankings</a>`,
+			`&bullet; <a href="https://www.smogon.com/forums/threads/3646736/">Anything Goes Sample Teams</a>`,
 		],
 
-		mod: 'gen8',
-		searchShow: false,
+		mod: 'gen7',
+		// searchShow: false,
 		ruleset: ['Obtainable', 'Team Preview', 'HP Percentage Mod', 'Cancel Mod', 'Endless Battle Clause'],
 	},
 ];
