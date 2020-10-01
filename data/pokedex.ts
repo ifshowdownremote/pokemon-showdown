627b34d9313a306203a306ec5ec5ae327ddfad63
root <root@vps726021.ovh.net>
2020-10-01 17:39:34 +0200

    test

--- data/pokedex.ts
+++ data/pokedex.ts
@@ -48017,7 +48017,7 @@ export const BattlePokedex: {[speciesid: string]: SpeciesData} = {
 		abilities: {0: "Blaze", 1: "No Guard", H: "Solar Power"},
 		heightm: 1.65,
 		weightkg: 110.25,
-		color: "Red",
+		color: "Green",
 		eggGroups: ["Human-Like", "Monster"],
 	},
 };
