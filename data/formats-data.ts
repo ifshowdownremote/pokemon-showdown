c0095b36e0c26c3a204a157917dbbb3b39738dd2
Pellevoisin <root@vps726021.ovh.net>
2020-10-03 20:08:18 +0200

    autobuild

--- data/formats-data.ts
+++ data/formats-data.ts
@@ -6094,4 +6094,355 @@ export const BattleFormatsData: {[k: string]: SpeciesFormatsData} = {
 	dragonitegyarados: {
 		tier: "UU",
 	},
+	charizardscyther: {
+		tier: "UU",
+	},
+	salamencemetapod: {
+		tier: "UU",
+	},
+	ivysaurivysaur: {
+		tier: "NFE",
+	},
+	chandelurebreloom: {
+		tier: "UU",
+	},
+	chandelurevolcarona: {
+		tier: "UU",
+	},
+	volcaronahydreigon: {
+		tier: "UU",
+	},
+	kyogrezapdos: {
+		tier: "Uber",
+	},
+	dialgaespeon: {
+		tier: "Uber",
+	},
+	zapdoskyogre: {
+		tier: "Uber",
+	},
+	clefablegroudon: {
+		tier: "Uber",
+	},
+	quagsiregroudon: {
+		tier: "Uber",
+	},
+	giratinaespeon: {
+		tier: "Uber",
+	},
+	dialgalugia: {
+		tier: "Uber",
+	},
+	dialgagiratina: {
+		tier: "Uber",
+	},
+	giratinalugia: {
+		tier: "Uber",
+	},
+	groudonlugia: {
+		tier: "Uber",
+	},
+	umbreonlugia: {
+		tier: "Uber",
+	},
+	porygon2lugia: {
+		tier: "Uber",
+	},
+	porygon2giratina: {
+		tier: "Uber",
+	},
+	dialgaporygon2: {
+		tier: "Uber",
+	},
+	giratinaporygon2: {
+		tier: "Uber",
+	},
+	kyogregroudon: {
+		tier: "Uber",
+	},
+	dialgahooh: {
+		tier: "Uber",
+	},
+	giratinahooh: {
+		tier: "Uber",
+	},
+	hoohlugia: {
+		tier: "Uber",
+	},
+	hoohgiratina: {
+		tier: "Uber",
+	},
+	hoohdialga: {
+		tier: "Uber",
+	},
+	giratinaferrothorn: {
+		tier: "Uber",
+	},
+	dialgaferrothorn: {
+		tier: "Uber",
+	},
+	ferrothornkyogre: {
+		tier: "Uber",
+	},
+	kyogreferrothorn: {
+		tier: "Uber",
+	},
+	groudonferrothorn: {
+		tier: "Uber",
+	},
+	sylveonarceus: {
+		tier: "Uber",
+	},
+	ferrothorngroudon: {
+		tier: "Uber",
+	},
+	sylveonrayquaza: {
+		tier: "Uber",
+	},
+	slakingazumarill: {
+		tier: "Uber",
+	},
+	rayquazasylveon: {
+		tier: "Uber",
+	},
+	groudonazumarill: {
+		tier: "Uber",
+	},
+	regigigasazumarill: {
+		tier: "Uber",
+	},
+	zekromazumarill: {
+		tier: "Uber",
+	},
+	arceusazumarill: {
+		tier: "Uber",
+	},
+	rayquazaazumarill: {
+		tier: "Uber",
+	},
+	metagrossazumarill: {
+		tier: "Uber",
+	},
+	mamoswineazumarill: {
+		tier: "Uber",
+	},
+	gliscorlugia: {
+		tier: "Uber",
+	},
+	giratinaforretress: {
+		tier: "Uber",
+	},
+	gliscorarceus: {
+		tier: "Uber",
+	},
+	feraligatrazumarill: {
+		tier: "Uber",
+	},
+	zekromblaziken: {
+		tier: "Uber",
+	},
+	blazikenzekrom: {
+		tier: "Uber",
+	},
+	rayquazablaziken: {
+		tier: "Uber",
+	},
+	blazikenrayquaza: {
+		tier: "Uber",
+	},
+	blazikenhooh: {
+		tier: "Uber",
+	},
+	kecleonarceus: {
+		tier: "Uber",
+	},
+	blazikenarceus: {
+		tier: "Uber",
+	},
+	hoohblaziken: {
+		tier: "Uber",
+	},
+	blazikenhitmonlee: {
+		tier: "Uber",
+	},
+	kecleonmew: {
+		tier: "Uber",
+	},
+	genesectgroudon: {
+		tier: "Uber",
+	},
+	mewtwodarkrai: {
+		tier: "Uber",
+	},
+	darkraimewtwo: {
+		tier: "Uber",
+	},
+	darkraideoxys: {
+		tier: "Uber",
+	},
+	deoxysdarkrai: {
+		tier: "Uber",
+	},
+	mewtwodeoxys: {
+		tier: "Uber",
+	},
+	lugiaarceus: {
+		tier: "Uber",
+	},
+	kingdragroudon: {
+		tier: "Uber",
+	},
+	deoxysmewtwo: {
+		tier: "Uber",
+	},
+	dragonitearceus: {
+		tier: "Uber",
+	},
+	arceusdragonite: {
+		tier: "Uber",
+	},
+	arceuslugia: {
+		tier: "Uber",
+	},
+	kingdrazekrom: {
+		tier: "Uber",
+	},
+	kyogrekingdra: {
+		tier: "Uber",
+	},
+	palkiakingdra: {
+		tier: "Uber",
+	},
+	leafeongroudon: {
+		tier: "Uber",
+	},
+	leafeonreshiram: {
+		tier: "Uber",
+	},
+	reshiramleafeon: {
+		tier: "Uber",
+	},
+	dialgagroudon: {
+		tier: "Uber",
+	},
+	mewtworeshiram: {
+		tier: "Uber",
+	},
+	groudondialga: {
+		tier: "Uber",
+	},
+	charizardmewtwo: {
+		tier: "Uber",
+	},
+	typhlosionmewtwo: {
+		tier: "Uber",
+	},
+	tyranitargroudon: {
+		tier: "Uber",
+	},
+	groudontyranitar: {
+		tier: "Uber",
+	},
+	palkiareshiram: {
+		tier: "Uber",
+	},
+	dialgatyranitar: {
+		tier: "Uber",
+	},
+	giratinatyranitar: {
+		tier: "Uber",
+	},
+	tyranitargiratina: {
+		tier: "Uber",
+	},
+	woopersandshrew: {
+		tier: "LC",
+	},
+	meowthmeowth: {
+		tier: "LC",
+	},
+	eeveemeowth: {
+		tier: "LC",
+	},
+	bisharpmeowth: {
+		tier: "LC",
+	},
+	meowtheevee: {
+		tier: "LC",
+	},
+	nosepassseel: {
+		tier: "LC",
+	},
+	piplupeevee: {
+		tier: "LC",
+	},
+	seelnosepass: {
+		tier: "LC",
+	},
+	machopaipom: {
+		tier: "LC",
+	},
+	natushieldon: {
+		tier: "LC",
+	},
+	chinchoumantyke: {
+		tier: "LC",
+	},
+	gligarmantyke: {
+		tier: "LC",
+	},
+	rhyhornmantyke: {
+		tier: "LC",
+	},
+	mantykegligar: {
+		tier: "LC",
+	},
+	sandshrewmantyke: {
+		tier: "LC",
+	},
+	phanpymantyke: {
+		tier: "LC",
+	},
+	torchicsneasel: {
+		tier: "LC",
+	},
+	sneaseltorchic: {
+		tier: "LC",
+	},
+	gastlyabra: {
+		tier: "LC",
+	},
+	abragastly: {
+		tier: "LC",
+	},
+	bidoofdratini: {
+		tier: "LC",
+	},
+	charmanderabra: {
+		tier: "LC",
+	},
+	bulbasaurcharmander: {
+		tier: "LC",
+	},
+	vulpixcyndaquil: {
+		tier: "LC",
+	},
+	cyndaquilvulpix: {
+		tier: "LC",
+	},
+	hitmonleetalonflame: {
+		tier: "OU",
+	},
+	clefabletangela: {
+		tier: "OU",
+	},
+	magmortarvenusaur: {
+		tier: "OU",
+	},
+	dialgavenusaur: {
+		tier: "OU",
+	},
+	chandelureinfernape: {
+		tier: "OU",
+	},
 };
