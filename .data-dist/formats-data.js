624a2c621ac18d83109d50416412ca31a0cca055
Pellevoisin <showdown@vps726021.ovh.net>
2020-10-06 11:17:54 +0200

    test

--- .data-dist/formats-data.js
+++ .data-dist/formats-data.js
@@ -1,6 +1,3 @@
 "use strict";Object.defineProperty(exports, "__esModule", {value: true}); const BattleFormatsData = {
-	bulbasaur: {
-		tier: "LC",
-	},
 }; exports.BattleFormatsData = BattleFormatsData;
 
--- .data-dist/pokedex.js
+++ .data-dist/pokedex.js
@@ -1,16 +1,3 @@
 "use strict";Object.defineProperty(exports, "__esModule", {value: true}); const BattlePokedex = {
-	bulbasaur: {
-		num: 1,
-		name: "Bulbasaur",
-		types: ["Grass", "Poison"],
-		genderRatio: {M: 0.875, F: 0.125},
-		baseStats: {hp: 45, atk: 49, def: 49, spa: 65, spd: 65, spe: 45},
-		abilities: {0: "Overgrow", H: "Chlorophyll"},
-		heightm: 0.7,
-		weightkg: 6.9,
-		color: "Green",
-		evos: ["Ivysaur"],
-		eggGroups: ["Monster", "Grass"],
-	},
 }; exports.BattlePokedex = BattlePokedex;
 
